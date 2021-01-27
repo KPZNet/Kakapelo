43e4dc2ac773e5d5cea99d478cdf235c16662935
kpznet <kpznet@outlook.com>
2021-01-27 15:35:52 -0800

    devs

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 
 class db_constants:
-    bucket = "Kwib"
+    bucket = "Katelo"
     token = "gLY5dHWzppci-UqOSUgVYgoGBv5nDTzkhZpK7TunPCXHLMdIVFTKFAv2Mf7QoL1D5m4mOiaGqzlaGRcoxkf9oQ=="
     org = "kpznet"
     username = "KenCeglia@hotmail.com"
